485fdbaa43e94eea5e4771f241a841b79eb928ce
hibiki4945 <wc4910720@gmail.com>
2023-11-22 14:49:20 +0800

    add leave-application's date&time-selecter

--- app/src/main/java/com/example/demo_06/mainnav/home/EmployeeLeaveApplicationFragment.kt
+++ app/src/main/java/com/example/demo_06/mainnav/home/EmployeeLeaveApplicationFragment.kt
@@ -1,14 +1,15 @@
 package com.example.demo_06.mainnav.home
 
-import android.R
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
-import androidx.navigation.NavOptions
-import androidx.navigation.fragment.findNavController
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import com.example.demo_06.base.BaseFragment
 import com.example.demo_06.databinding.FragmentEmployeeLeaveApplicationBinding
 import com.example.mvvm_learning.setruth.mvvmlearn.viewmodeled.PublicViewModel
-
+import java.util.Calendar
 
 class EmployeeLeaveApplicationFragment: BaseFragment<FragmentEmployeeLeaveApplicationBinding, PublicViewModel>(
     FragmentEmployeeLeaveApplicationBinding::inflate,
@@ -42,6 +43,72 @@ class EmployeeLeaveApplicationFragment: BaseFragment<FragmentEmployeeLeaveApplic
 //                null,
 //                NavOptions.Builder().setPopUpTo(com.example.demo_06.R.id.manageNavFragment,true).build())
 //        }
+
+        binding.startDate.setOnClickListener{
+            val calendar: Calendar = Calendar.getInstance()
+            val year: Int = calendar.get(Calendar.YEAR)
+            val month: Int = calendar.get(Calendar.MONTH)
+            val day: Int = calendar.get(Calendar.DAY_OF_MONTH)
+//          選択した日付を表示する
+            DatePickerDialog(it.context,
+                { view, year, month, day ->
+                    val datetime = "$year-$month-$day"
+                    binding.startDate?.setText(datetime)
+                }, year, month, day
+            ).show()
+        }
+
+        binding.startTime.setOnClickListener{
+            val calendar: Calendar = Calendar.getInstance()
+            val hourOfDay: Int = calendar.get(Calendar.HOUR)
+            val minute: Int = calendar.get(Calendar.MINUTE)
+//          選択した時間を表示する
+            TimePickerDialog(it.context,
+                { view, hourOfDay, minute ->
+                    val datetime = "$hourOfDay:$minute"
+                    binding.startTime?.setText(datetime)
+                }, hourOfDay, minute, true
+            ).show()
+        }
+
+        binding.endDate.setOnClickListener{
+            val calendar: Calendar = Calendar.getInstance()
+            val year: Int = calendar.get(Calendar.YEAR)
+            val month: Int = calendar.get(Calendar.MONTH)
+            val day: Int = calendar.get(Calendar.DAY_OF_MONTH)
+//          選択した日付を表示する
+            DatePickerDialog(it.context,
+                { view, year, month, day ->
+                    val datetime = "$year-$month-$day"
+                    binding.endDate?.setText(datetime)
+                }, year, month, day
+            ).show()
+        }
+
+        binding.endTime.setOnClickListener{
+            val calendar: Calendar = Calendar.getInstance()
+            val hourOfDay: Int = calendar.get(Calendar.HOUR)
+            val minute: Int = calendar.get(Calendar.MINUTE)
+//          選択した時間を表示する
+            TimePickerDialog(it.context,
+                { view, hourOfDay, minute ->
+                    val datetime = "$hourOfDay:$minute"
+                    binding.endTime?.setText(datetime)
+                }, hourOfDay, minute, true
+            ).show()
+        }
+
+        binding.employeeLeaveSubmit.setOnClickListener{
+            var startDate = binding.startDate.text.toString()
+            var startTime = binding.startTime.text.toString()
+
+            Toast.makeText(
+                requireContext(),
+                startDate+startTime,
+                Toast.LENGTH_SHORT).show()
+
+        }
+
     }
 
 }
